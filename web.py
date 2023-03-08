593e6b5e5a72966c1827a2802a402774b1c14394
Aszzc <107671508+Aszzc@users.noreply.github.com>
2023-03-08 14:28:31 +0800

    Update web.py

--- web.py
+++ web.py
@@ -4,25 +4,39 @@ import datetime
 import requests
 from parsel import Selector
 
+def main():
+    st.set_page_config(page_title='东方财富研报查询', page_icon=None, layout="wide")
+    if 'selected_date' not in st.session_state:st.session_state['selected_date'] = datetime.date.today()
+    st.sidebar.date_input('选择查询日期',key='selected_date',on_change = query(st.session_state.selected_date))
+
+
 def processlink(link):
     resp = requests.get(link)
     sl = Selector(resp.text)
     pdf_url = sl.xpath('//a[@class = "pdf-link"]/@href').extract_first()
     return pdf_url
 
-@st.cache_data 
+
+
+@st.cache_data    
 def query(selected_date):
+    
     st.write(selected_date)
+    c1,c2,c3 = st.columns(3, gap="small")
+    
     qtype = [0,1,2,3,4]
     dbtype = {"0":"list","1":"list","2":"list","3":"jg","4":"jg",}
+    
     for i in qtype:
+        
         run_flag = True
         pageno = 1
-
+        
         while run_flag:
             url = 'https://reportapi.eastmoney.com/report/{0}?pageSize=100&beginTime={1}&endTime={1}&pageNo={2}&qType={3}'.format(\
             dbtype[str(i)],str(selected_date),str(pageno),str(i))
             resp = requests.get(url).json()
+            
             for j in resp['data']:
                 title = j['title']
                 orgSName = j['orgSName']
@@ -37,25 +51,17 @@ def query(selected_date):
                     infoCode = j["encodeUrl"]
                     link = 'https://data.eastmoney.com/report/zw_macresearch.jshtml?encodeUrl={0}'.format(infoCode)
                     link = processlink(link)
-
-                st.write('{0} {1} {2} {3}'.format(stockName,industryName,title,link))
+                    
+                if i == 0:
+                    c1.write('{0} {1} {2} {3}'.format(stockName,industryName,title,link))
+                elif i== 1:
+                    c2.write('{0} {1} {2} {3}'.format(stockName,industryName,title,link))
+                else:
+                    c3.write('{0} {1} {2} {3}'.format(stockName,industryName,title,link))
                 
             if resp['hits']/100 > pageno:
                 pageno = pageno+1
             else:
                 run_flag = False
-
-st.set_page_config(page_title='东方财富研报查询', page_icon=None, layout="wide")
-if 'selected_date' not in st.session_state:st.session_state['selected_date'] = datetime.date.today()
-st.sidebar.date_input('选择查询日期',key='selected_date',on_change = query(st.session_state.selected_date))
-
-
-    
-
-
-
-
-
-
-    
-
+                
+main()
