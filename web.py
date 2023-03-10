44f83508872c8dac2866733b7e0dfc8faffd2a36
Aszzc <107671508+Aszzc@users.noreply.github.com>
2023-03-10 09:52:22 +0800

    Update web.py

--- web.py
+++ web.py
@@ -1,45 +1,24 @@
+import requests
 import streamlit as st
 import datetime
-import requests
-from parsel import Selector
+import json
 
-def main():
-    st.set_page_config(page_title='东方财富研报查询', page_icon=None, layout="wide")
-    if 'selected_date' not in st.session_state:st.session_state['selected_date'] = datetime.date.today()
-    st.sidebar.date_input('选择查询日期',key='selected_date',on_change = query(st.session_state.selected_date))
-
-
-def processlink(link):
-    resp = requests.get(link)
-    sl = Selector(resp.text)
-    pdf_url = sl.xpath('//a[@class = "pdf-link"]/@href').extract_first()
-    return pdf_url
-
-
-
-@st.cache_data(ttl=600)   
-def query(selected_date):
-    
-    st.write(selected_date)
-    c1,c2,c3 = st.columns(3, gap="small")
-    
-    qtype = [0,1,2,3]
-    dbtype = {"0":"list","1":"list","2":"list","3":"jg","4":"jg",}
-    
+@st.cache_data(ttl=600)
+def query(date):
+    data = []
+    qtype = [0, 1, 2, 3]
+    dbtype = {"0": "list", "1": "list", "2": "list", "3": "jg", }
     for i in qtype:
-        
-        run_flag = True
         pageno = 1
-        
+        run_flag = True
         while run_flag:
-            url = 'https://reportapi.eastmoney.com/report/{0}?pageSize=100&beginTime={1}&endTime={1}&pageNo={2}&qType={3}'.format(\
-            dbtype[str(i)],str(selected_date),str(pageno),str(i))
+            url = 'https://reportapi.eastmoney.com/report/{0}?pageSize=100&beginTime={1}&endTime={1}&pageNo={2}&qType={3}'.format( \
+                dbtype[str(i)], str(date), str(pageno), str(i))
             resp = requests.get(url).json()
-            
+
             for j in resp['data']:
                 title = j['title']
                 orgSName = j['orgSName']
-                publishDate = j['publishDate']
                 industryName = j['industryName']
                 try:
                     stockName = j['stockName']
@@ -49,18 +28,33 @@ def query(selected_date):
                     stockName = "None"
                     infoCode = j["encodeUrl"]
                     link = 'https://data.eastmoney.com/report/zw_macresearch.jshtml?encodeUrl={0}'.format(infoCode)
-                    link = processlink(link)
-                    
-                if i == 0:
-                    c1.write('{0} {1} {2} {3}'.format(stockName,industryName,title,link))
-                elif i== 1:
-                    c2.write('{0} {1} {2} {3}'.format(stockName,industryName,title,link))
-                else:
-                    c3.write('{0} {1} {2} {3}'.format(stockName,industryName,title,link))
-                
-            if resp['hits']/100 > pageno:
-                pageno = pageno+1
-            else:
-                run_flag = False
-                
-main()
+
+                data.append({"type": i,"data": "{0} {1} {2} {3} {4}".format(stockName, industryName, title, orgSName, link)})
+            if (resp['hits']==0/ 100 > pageno):pageno = pageno + 1
+            else:run_flag = False
+    return data
+
+
+def func_date_change(date):
+    data = query(date)
+    c1,c2,c3 = st.columns(3)
+
+    for i in data:
+        if i['type'] == 0:c1.write(i['data'])
+        elif i['type'] == 1:c2.write(i['data'])
+        else:c3.write(i['data'])
+
+def download():
+    data = query(st.session_state['selected_time'])
+    data = json.dumps(data,ensure_ascii=False)
+    return data
+
+
+st.set_page_config(page_title='东方财富研报查询', page_icon=None, layout="wide")
+
+
+
+if 'selected_time' not in st.session_state:
+    st.session_state['selected_time'] = datetime.date.today()
+st.sidebar.date_input('选择查询日期',key='selected_time',on_change = func_date_change(st.session_state['selected_time']))
+st.sidebar.download_button('下载当前查询数据',data=download(),file_name='{0}.txt'.format(st.session_state['selected_time']))
