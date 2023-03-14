95cbf6239ed1ad8890063cab20f386ee15fd397d
Aszzc <107671508+Aszzc@users.noreply.github.com>
2023-03-14 08:58:27 +0800

    Update stocks.py

--- pages/stocks.py
+++ pages/stocks.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-components.iframe('https://docs.streamlit.io/en/latest')
+components.iframe('http://www.iwencai.com/unifiedwap/result?w=低位，量升价缩，非st，非创业板，非科创版，股价低于20大于4，主板上市&querytype=stock')
