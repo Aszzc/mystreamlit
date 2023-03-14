99b83c4f69eb7ade59ccaf7c5c222d3d4dba25cf
Aszzc <107671508+Aszzc@users.noreply.github.com>
2023-03-14 08:57:32 +0800

    Update stocks.py

--- pages/stocks.py
+++ pages/stocks.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-components.iframe('http://www.iwencai.com/unifiedwap/result?w=%E4%BD%8E%E4%BD%8D%EF%BC%8C%E9%87%8F%E5%8D%87%E4%BB%B7%E7%BC%A9%EF%BC%8C%E9%9D%9Est%EF%BC%8C%E9%9D%9E%E5%88%9B%E4%B8%9A%E6%9D%BF%EF%BC%8C%E9%9D%9E%E7%A7%91%E5%88%9B%E7%89%88%EF%BC%8C%E8%82%A1%E4%BB%B7%E4%BD%8E%E4%BA%8E20%E5%A4%A7%E4%BA%8E4%EF%BC%8C%E4%B8%BB%E6%9D%BF%E4%B8%8A%E5%B8%82&querytype=stock')
+components.iframe('https://docs.streamlit.io/en/latest')
